59616c59e23582e02a9e047af8664d1fa9270933
qegbetita <133830949+qegbetita@users.noreply.github.com>
2023-05-18 21:53:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @st.cache_resource
 def load_model():
-  model=tf.keras.models.load_model('model.hdf5')
+  model = tf.keras.models.load_model('model.hdf5')
   return model
 model=load_model()
 st.write("""
